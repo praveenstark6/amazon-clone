01098082605961c0013bf964a4ce64ce9bc053e1
praveenstark6 <praveentony15@gmail.com>
2022-06-07 11:51:51 +0530

    add firebase.js for firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5UbmXV_ovUWco84y4C236jhRhuvQcgng",
+  authDomain: "clone-praveen.firebaseapp.com",
+  projectId: "clone-praveen",
+  storageBucket: "clone-praveen.appspot.com",
+  messagingSenderId: "88033655713",
+  appId: "1:88033655713:web:5ff9f7d8c7ecbac271e9e0",
+  measurementId: "G-E2Y74J0R8Y"
+};
